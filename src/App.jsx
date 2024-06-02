f51a74c989e3ca918cf816d2ed6a782be15e7d42
Mahadi Hasan <muhammadmahdihasan1@gmail.com>
2024-06-03 00:06:33 +0600

    done

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Features from "./components/Features"
+import Footer from "./components/Footer"
+import Hero from "./components/Hero"
+import Highlight from "./components/Highlights"
+import HowItWorks from "./components/HowItWorks"
+import Model from "./components/Model"
+import Navbar from "./components/Navbar"
+function App() {
+  return (
+    <>
+     <Navbar/>
+     <Hero/>
+     <Highlight/>
+     <Model />
+     <Features/>
+     <HowItWorks/>
+     <Footer/>
+    </>
+  )
+}
+
+export default App
